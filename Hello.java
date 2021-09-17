f9d68ca90d124c99bede8091bd67e0b229198d91
ImSandy471 <84791586+ImSandy471@users.noreply.github.com>
2021-09-17 09:59:34 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,3 @@
-# hello-world
-#This my very first repository creation, while learning github
-#I am trying to learn the branch concept in the GitHub.
-
 public class Hello {
   public static void main(string[] args) {
     // Prints "Hello, World" in the terminal window.
